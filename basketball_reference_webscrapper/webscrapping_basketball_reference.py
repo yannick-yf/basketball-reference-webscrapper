c39f59a3ae2d9c6a4cf325c74bf99657cfe770a9
Yannick Flores <yannick.flores1992@gmail.com>
2024-05-28 13:52:33 +0200

    0.1.6 - params file integration and pylint score

--- basketball_reference_webscrapper/webscrapping_basketball_reference.py
+++ basketball_reference_webscrapper/webscrapping_basketball_reference.py
@@ -1,163 +1,148 @@
+"""
+Class that take as inputs url and season and return all games teams stats
+"""
+
 from dataclasses import dataclass
 from urllib.request import urlopen
+import time
 from bs4 import BeautifulSoup
 import pandas as pd
 import requests
-import time
-import importlib_resources
 
-from basketball_reference_webscrapper.data_models.feature_model import FeatureIn, FeatureOut
+import importlib_resources
+import yaml
+
+from basketball_reference_webscrapper.data_models.feature_model import (
+    FeatureIn
+)
 from basketball_reference_webscrapper.utils.logs import get_logger
 
 logger = get_logger("WEB_SCRAPPING_EXECUTION", log_level="INFO")
+
 
 @dataclass
 class WebScrapBasketballReference:
     """
     Class that take as inputs url and season and return all games teams stats
     """
-    
+
     def __init__(self, feature_object: FeatureIn) -> None:
+        """
+        Init function
+        """
         self.feature_object = feature_object
-    
-    def _get_team_list_values_validation(self, team_abbrev_list: list):
-        if  (isinstance(self.feature_object.team, list)) and (all(isinstance(s, str) for s in self.feature_object.team)):
-            if set(self.feature_object.team).issubset(team_abbrev_list)==False:
-                raise ValueError(
-                    "team list arg provided is not accepted.\
-                    Value needs to be 'all' or a NBA team abbreviation such as BOS for Boston Celtics.\
-                    More details on all values possible in the GitHub Repo docs"
-                )
-        elif isinstance(self.feature_object.team, str):
-            if (self.feature_object.team != 'all') and (self.feature_object.team not in team_abbrev_list):
-                raise ValueError(
-                    "team arg provided is not accepted.\
-                    Value needs to be 'all' or a NBA team abbreviation such as BOS for Boston Celtics.\
-                    More details on all values possible in the GitHub Repo docs"
-                )
-        else:
-            raise ValueError(
-                    "team args should be a string or a list of string.\
-                    Value needs to be NBA team abbreviation such as BOS for Boston Celtics.\
-                    More details on all values possible in the GitHub Repo docs"
-                )
 
     def webscrappe_nba_games_data(self):
         """
         Webscrappe NBA games data
         """
 
-        #------------------------------------------------------
-        # Get team reference data 
+        # ------------------------------------------------------
+        # User Input check and validation
+        self._get_data_type_validation()
+        self._get_season_validation()
+
+        # ------------------------------------------------------
+        # Import params file
+        with open("basketball_reference_webscrapper/params.yaml", encoding="utf-8") as conf_file:
+            config = yaml.safe_load(conf_file)
+
+        # ------------------------------------------------------
+        # Get team reference data
         ref = (
             importlib_resources.files("basketball_reference_webscrapper")
             / "constants/team_city_refdata.csv"
         )
         with importlib_resources.as_file(ref) as path:
-            team_city_refdata = pd.read_csv(path, sep = ';')
+            team_city_refdata = pd.read_csv(path, sep=";")
 
-        #------------------------------------------------------
-        # User Input check and validation
-        self._get_data_type_validation()
-        self._get_season_validation()
-        self._get_team_list_values_validation(list(team_city_refdata['team_abrev']))
+        # ------------------------------------------------------
+        # Part2 - User Input check and validation
+        self._get_team_list_values_validation(list(team_city_refdata["team_abrev"]))
 
-        #------------------------------------------------------
+        # ------------------------------------------------------
         # Team filtering
         if isinstance(self.feature_object.team, list):
-            team_city_refdata = team_city_refdata[team_city_refdata['team_abrev'].isin(self.feature_object.team)]
+            team_city_refdata = team_city_refdata[
+                team_city_refdata["team_abrev"].isin(self.feature_object.team)
+            ]
         elif isinstance(self.feature_object.team, str):
-            if self.feature_object.team != 'all':
-                team_city_refdata = team_city_refdata[team_city_refdata['team_abrev'] == self.feature_object.team]
-        
-        #------------------------------------------------------
+            if self.feature_object.team != "all":
+                team_city_refdata = team_city_refdata[
+                    team_city_refdata["team_abrev"] == self.feature_object.team
+                ]
+
+        # ------------------------------------------------------
         # Initialization of the dataframe to fill-in
         games = pd.DataFrame()
 
-        #------------------------------------------------------
+        # ------------------------------------------------------
         # For Loop Throught all the team abrev for the given season
         for index, row in team_city_refdata.iterrows():
 
             # URL to scrape
-            team = row['team_abrev']
+            team = row["team_abrev"]
 
-            logger.info(
-                f"Execution for {team}"
-            )
-            
-            if self.feature_object.data_type == 'gamelog':
-                url = "https://www.basketball-reference.com/teams/" +\
-                    team + "/" +\
-                    str(self.feature_object.season) +\
-                    "/"+\
-                    self.feature_object.data_type
+            logger.info("Execution for: %s", team)
+
+            if self.feature_object.data_type == "gamelog":
+                url = (
+                    config[self.feature_object.data_type]["url"]
+                    + team
+                    + "/"
+                    + str(self.feature_object.season)
+                    + "/"
+                    + self.feature_object.data_type
+                )
             else:
-                url = "https://www.basketball-reference.com/teams/ATL/2022_games.html"
+                url = (
+                    config[self.feature_object.data_type]["url"]
+                    + team
+                    + "/"
+                    + str(self.feature_object.season)
+                    + "_games.html"
+                )
 
             # url = f'https://www.basketball-reference.com/players/{player_id}/gamelog/{id_season}'
             # url = f"https://www.basketball-reference.com/teams/{team}/{SEASON}.html"
-            # # url = f"https://www.basketball-reference.com/teams/BRK/2001.html"
+            # url = f"https://www.basketball-reference.com/teams/BRK/2001.html"
 
-            if '200' in str(requests.get(url)):
-
+            if "200" in str(requests.get(url, timeout=60)):
                 # collect HTML data and create beautiful soup object:
-                html = urlopen(url)
-                        
-                # create beautiful soup object from HTML
-                soup = BeautifulSoup(html, "html.parser")
+                # html = urlopen(url)
 
+                with urlopen(url) as html:
 
-                if self.feature_object.data_type == 'gamelog':
-                    rows = soup.findAll('tr')[2:]
-                else:
-                    rows = soup.findAll('tr')[1:]
+                    # create beautiful soup object from HTML
+                    soup = BeautifulSoup(html, "html.parser")
 
-                rows_data = [[td.getText() for td in rows[i].findAll('td')]
-                                    for i in range(len(rows))]
+                    rows = soup.findAll("tr")[
+                        config[self.feature_object.data_type]["beautifulsoup_tr_index"] :
+                    ]
 
-                if len(rows_data) != 0:
-                    # create the dataframe
-                    games_tmp = pd.DataFrame(rows_data)
+                    rows_data = [
+                        [td.getText() for td in rows[i].findAll("td")]
+                        for i in range(len(rows))
+                    ]
 
-                    if self.feature_object.data_type == 'gamelog':
-                        cols = ["game_nb", "game_date", "extdom", "opp", "results",
-                            "pts_tm","pts_opp",
-                            "fg_tm", "fga_tm","fg_prct_tm",
-                            "3p_tm","3pa_tm", "3p_prct_tm","ft_tm","fta_tm","ft_prct_tm",
-                            "orb_tm","trb_tm", "ast_tm","stl_tm","blk_tm" ,"tov_tm","pf_tm",
-                            "nc",
-                            "fg_opp","fga_opp","fg_prct_opp",
-                            "3p_opp", "3pa_opp", "3p_prct_opp", "ft_opp", "fta_opp","ft_prct_opp",
-                            "orb_opp", "trb_opp","ast_opp", "stl_opp", "blk_opp","tov_opp", "pf_opp"]
-                    else:
-                        cols = ['game_date', 'time_start', 'nc1', 'nc2', 'extdom', 'opponent', 'w_l', 'overtime', 'pts_tm', 'pts_opp', 'w_tot', 'l_tot', 'streak_w_l', 'nc3']
-                    
-                    games_tmp.columns =  cols
-                    games_tmp = games_tmp.dropna()
-                    games_tmp['id_season'] = self.feature_object.season
-                    games_tmp['tm'] = team
-                    games = pd.concat([games, games_tmp], axis=0)
-            
+                    if len(rows_data) != 0:
+
+                        games_tmp = pd.DataFrame(rows_data)
+                        games_tmp.columns = config[self.feature_object.data_type]["list_columns"]
+                        games_tmp = games_tmp.dropna()
+                        games_tmp.loc[:, "id_season"] = self.feature_object.season
+                        games_tmp.loc[:, "tm"] = team
+                        games = pd.concat([games, games_tmp], axis=0)
+
             time.sleep(5)
 
-        if self.feature_object.data_type == 'gamelog':
-            games = games[[
-                'id_season', 'game_nb', 'game_date', 'extdom', 'tm','opp', 'results', 'pts_tm', 'pts_opp',
-                'fg_tm', 'fga_tm', 'fg_prct_tm', '3p_tm', '3pa_tm', '3p_prct_tm',
-                'ft_tm', 'fta_tm', 'ft_prct_tm', 'orb_tm', 'trb_tm', 'ast_tm', 'stl_tm',
-                'blk_tm', 'tov_tm', 'pf_tm', 'fg_opp', 'fga_opp', 'fg_prct_opp',
-                '3p_opp', '3pa_opp', '3p_prct_opp', 'ft_opp', 'fta_opp', 'ft_prct_opp',
-                'orb_opp', 'trb_opp', 'ast_opp', 'stl_opp', 'blk_opp', 'tov_opp',
-                'pf_opp']]
-        else:
-            games = games[['id_season', 'tm', 'game_date', 'time_start', 'extdom', 'opponent', 'w_l', 'overtime', 'pts_tm', 'pts_opp', 'w_tot', 'l_tot', 'streak_w_l']]
-            
+        games = games[config[self.feature_object.data_type]["list_columns_to_select"]]
+
         return games
-    
 
     def _get_data_type_validation(self):
         if self.feature_object.data_type is not None:
-            if str(self.feature_object.data_type) not in ['gamelog', 'schedule']:
+            if str(self.feature_object.data_type) not in ["gamelog", "schedule"]:
                 raise ValueError(
                     "data_type value provided is not supported by the package.\
                     Accepted values are: 'gamelog', 'schedule'.\
@@ -165,32 +150,48 @@ class WebScrapBasketballReference:
                 )
         else:
             raise ValueError(
-                    "data_type is a required argument.\
+                "data_type is a required argument.\
                     Accepted values are: 'gamelog', 'schedule'.\
                     Read documentation for more details"
-                )
-    
+            )
+
     def _get_season_validation(self):
         if isinstance(self.feature_object.season, int):
-            if (
-                self.feature_object.season < 1999
-            ):
+            if self.feature_object.season < 1999:
                 raise ValueError(
                     "season value provided is a year not supported by the package. \
                     it should be between 2000 and current NBA season."
                 )
         else:
             raise ValueError(
-                    "season is a required argument and should be an int value between 2000 and current NBA season."
+                "season is a required argument and should be an int value between 2000\
+                    and current NBA season."
+            )
+
+    def _get_team_list_values_validation(self, team_abbrev_list: list):
+        if (isinstance(self.feature_object.team, list)) and (
+            all(isinstance(s, str) for s in self.feature_object.team)
+        ):
+            if set(self.feature_object.team).issubset(team_abbrev_list) is False:
+                raise ValueError(
+                    "team list arg provided is not accepted.\
+                    Value needs to be 'all' or a NBA team abbreviation\
+                    such as BOS for Boston Celtics.\
+                    More details on all values possible in the GitHub Repo docs"
                 )
-    
-
-    
-if __name__ == "__main__":
-    webscrapping_class = WebScrapBasketballReference(
-        url = "test",
-        season = 2022
-    )
-
-    nba_games = webscrapping_class.webscrappe_nba_games_data()
-
+        elif isinstance(self.feature_object.team, str):
+            if (self.feature_object.team != "all") and (
+                self.feature_object.team not in team_abbrev_list
+            ):
+                raise ValueError(
+                    "team arg provided is not accepted.\
+                    Value needs to be 'all' or a NBA team abbreviation\
+                    such as BOS for Boston Celtics.\
+                    More details on all values possible in the GitHub Repo docs"
+                )
+        else:
+            raise ValueError(
+                "team args should be a string or a list of string.\
+                    Value needs to be NBA team abbreviation such as BOS for Boston Celtics.\
+                    More details on all values possible in the GitHub Repo docs"
+            )
