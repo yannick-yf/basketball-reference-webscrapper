1ff7a292f0823533d615ef8ccd78426e70ea2705
Yannick Flores <yannick.flores1992@gmail.com>
2025-11-10 12:26:34 +0100

    Increases package version and adjust headers
    Increases the package version to reflect the
    changes. Comments out specific headers to use
    default values.

--- basketball_reference_webscrapper/webscrapping_basketball_reference.py
+++ basketball_reference_webscrapper/webscrapping_basketball_reference.py
@@ -128,16 +128,16 @@ class WebScrapBasketballReference:
                     req = Request(url, headers={
                         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
                         'Referer': 'https://www.basketball-reference.com/',
-                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-                        'Accept-Language': 'en-US,en;q=0.5',
-                        'Accept-Encoding': 'gzip, deflate, br',
-                        'DNT': '1',
-                        'Connection': 'keep-alive',
-                        'Upgrade-Insecure-Requests': '1',
-                        'Sec-Fetch-Dest': 'document',
-                        'Sec-Fetch-Mode': 'navigate',
-                        'Sec-Fetch-Site': 'none',
-                        'Cache-Control': 'max-age=0',
+                        # 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                        # 'Accept-Language': 'en-US,en;q=0.5',
+                        # 'Accept-Encoding': 'gzip, deflate, br',
+                        # 'DNT': '1',
+                        # 'Connection': 'keep-alive',
+                        # 'Upgrade-Insecure-Requests': '1',
+                        # 'Sec-Fetch-Dest': 'document',
+                        # 'Sec-Fetch-Mode': 'navigate',
+                        # 'Sec-Fetch-Site': 'none',
+                        # 'Cache-Control': 'max-age=0',
                     })
                     
                     with urlopen(req) as html:
